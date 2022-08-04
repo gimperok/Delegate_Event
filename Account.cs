d33b75fa62b6c2ae1b30f66a1893b5d1a851401d
Alexei <gimp24@mail.ru>
2022-08-04 11:24:02 +0300

    Добавьте файлы проекта.

---
+++ Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Event
+{
+    internal class Account
+    {
+        public int Sum { get; set; }  //свойство для хранения суммы на счете
+
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+
+        public event AccountHandler? Notify;  //создаем событие
+
+        internal void Put(int sum)  //метод пополнения баланса
+        {
+            Sum += sum;
+            Notify?.Invoke($"На ваш счет поступило {sum}$. Ваш баланс: {Sum}");  //вызываем событие
+        }
+
+        internal void Take(int sum)  // метод для снятия валюты
+        {
+            if (sum <= Sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"С вашего счета было снято: {sum}$. Баланс: {Sum}.");  //если удалось снять сумму, вызываем событие
+            }
+            else Notify?.Invoke($"Вы пытаетесь снять {sum}$. Недостаточно средств! Ваш баланс {Sum}$.");  //если сумму снять не удалось, вызываем событие
+        }
+
+        public delegate void AccountHandler(string message);  //создаем делегат, который принимает сообщение
+    }
+}
